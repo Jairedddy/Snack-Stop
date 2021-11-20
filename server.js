05b9dbb0de8fab13932391687f418ac1a54311eb
Jaishuk P <jaishukreddy7@gmail.com>
2021-11-20 21:55:37 +0530

    add login

--- server.js
+++ server.js
@@ -5,15 +5,28 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3300
 
-app.get('/',function(req,res){
-    res.render('home')
-}) 
 
 app.use(express.static('public'))
 
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',function(req,res){
+    res.render('home')
+});
+
+app.get('/cart',function(req,res){
+    res.render('cart')
+});
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
+
+app.get('/register',function(req,res){
+    res.render('register')
+})
 
 app.listen(PORT , () =>{
     console.log(`Listening on port ${PORT}`);
