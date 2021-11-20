ec967cb8bb26d55eca89e90dec05272f410ebfe4
Jaishuk P <jaishukreddy7@gmail.com>
2021-11-20 20:16:20 +0530

    'initial'

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+console.log("hello from app js");
+
+/***/ }),
+
+/***/ "./resources/scss/app.scss":
+/*!*********************************!*\
+  !*** ./resources/scss/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: \"+\" and \"-\" must be surrounded by whitespace in calculations.\n    ╷\n329 │     top : calc(100%-12rem);\r\n    │                    ^\n    ╵\n  resources\\scss\\app.scss 329:20  root stylesheet\n    at processResult (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\webpack\\lib\\NormalModule.js:751:19)\n    at C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\webpack\\lib\\NormalModule.js:853:5\n    at C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\sass\\sass.dart.js:96399:16)\n    at render_closure1.call$2 (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\sass\\sass.dart.js:82305:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\sass\\sass.dart.js:28284:18)\n    at _FutureListener.handleError$1 (C:\\Users\\JAISHUK REDDY\\Desktop\\WEB TECH\\webtech project\\node_modules\\sass\\sass.dart.js:26806:21)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/scss/app.scss"]();
+/******/ 	
+/******/ })()
+;
